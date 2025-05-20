e7a51cabb4977fa99967ad76175f74d69d7e3f53
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2025-05-20 18:16:51 +0200

    Add fylker to all files

--- kommune/setup.R
+++ kommune/setup.R
@@ -90,14 +90,27 @@ recode_komm <- function(dt, code, col){
 
 dd <- recode_komm(dx1, komm, "kommunenr")
 
+## Function --------------
+komm <- data.table::fread("kommuner2018_til_2023.csv")
 ## Colnames for kommunenr in different files
 komNames <- c("kommunenr", "Utvalg_Kommunenummer",
               "kommunenr", "Utvalg_Kommunenummer",
               "Utvalg_Kommunenummer", "utvalg_kommunenummer")
 
 
-for (i in seq_along(files)){
-  d <- DD[[i]]
-  x <- komNames[i]
-  recode_komm(d, komm, x)
+DD <- mapply(
+  FUN = recode_komm,
+  dt = DD,
+  col = komNames,
+  MoreArgs = list(code = komm),
+  SIMPLIFY = FALSE
+)
+
+fylke <- data.table::fread("kommune_fylke2023.csv")
+
+for (i in seq_along(DD)){
+  DD[[i]][fylke, on = .(newKomm = targetCode), newFylke := sourceCode]
 }
+
+newFiles <- paste("New", fname, sep = "_")
+mapply(function(data, fname) export(data, fname), DD, newFiles)
