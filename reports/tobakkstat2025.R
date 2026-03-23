d35a93fadc447f579738304a3417f2682a0acf4b
Yusman Bin Kamaleri <Yusman.Bin.Kamaleri@helsedir.no>
2026-03-23 15:32:18 +0100

    Add stacked diagram

--- reports/tobakkstat2025.R
+++ reports/tobakkstat2025.R
@@ -139,53 +139,130 @@ dualDT <- data.table::rbindlist(list(rs, re, se, rse))
 dt <- torr::group_age(dt, var = "alder",
                    breaks = c(16, 25, 55, Inf),
                    labels = c("16-24", "25-54", "55-79"),
-                   new_var = "age3",
+                   new_var = "age3str",
                    copy = FALSE)
 
+idxAge <- dt[, .N, keyby = age3str][, idxAge := .I]
+dt[idxAge, on = "age3str", idxAge := i.idxAge]
+
+
+source("https://raw.githubusercontent.com/folkehelsestats/toa/refs/heads/main/rusund/functions/fun-percent-weighted.R")
+
+## Denominator -----------------------------------------------------------------
+## Only those who use at lease one of the three
+dtt <- dt[popSub == 1]
+
 # Røyk + snus
-rsAge <- calc_percentage_total_ci(dt,
-                               outcome_var = "dual_rs",
-                               group_vars = "age3",
-                               denominator_var = "popSub",
-                               weight_var = "vekt",
-                               include_total = FALSE
-                               )
+
+dtt[combi3 == 3, dual_rs := 3]
+rsAge <- proscat_weighted(x = "idxAge", "dual_rs", weight = "vekt", d = dtt, total = TRUE)
+rsAge[idxAge, on = "idxAge", alder := i.age3str]
+rsAge[is.na(alder), alder := "Totalt"]
+rsAge <- rsAge[dual_rs != 0]
 
 rsAge[, type := "Snus og sigarettar"]
+rsAge[, type2 := "rs"]
+
+colx <- c("idxAge", "weighted_count", "sum")
+rsAge[, (colx) := NULL]
+data.table::setnames(rsAge, "dual_rs", "dual")
+
+rsWide <- dcast(rsAge, alder ~ dual, value.var = "percentage")
+
+highchart() |>
+  hc_chart(type = "column") |>
+  hc_xAxis(categories = rsWide$alder) |>
+  hc_plotOptions(column = list(stacking = "normal"))|>
+  hc_add_series(name = "dual", data = rsWide$`1`, stack = "stack1")
+
+
 
 # Røyk + e-sig
-reAge <- calc_percentage_total_ci(dt,
-                               outcome_var = "dual_re",
-                               group_vars = "age3",
-                               denominator_var = "popSub",
-                               weight_var = "vekt",
-                               include_total = FALSE
-                               )
+
+dtt[combi3 == 3, dual_re := 3]
+reAge <- proscat_weighted(x = "idxAge", "dual_re", weight = "vekt", d = dtt, total = TRUE)
+reAge[idxAge, on = "idxAge", alder := i.age3str]
+reAge[is.na(alder), alder := "Totalt"]
+reAge <- reAge[dual_re != 0]
 
 reAge[, type := "Vipe og sigarettar"]
+reAge[, type2 := "re"]
+
+colx <- c("idxAge", "weighted_count", "sum")
+reAge[, (colx) := NULL]
+data.table::setnames(reAge, "dual_re", "dual")
+
+reWide <- dcast(reAge, alder ~ dual, value.var = "percentage")
 
 # Snus + e-sig
-seAge <- calc_percentage_total_ci(dt,
-                               outcome_var = "dual_se",
-                               group_vars = "age3",
-                               denominator_var = "popSub",
-                               weight_var = "vekt",
-                               include_total = FALSE
-                               )
+
+dtt[combi3 == 3, dual_se := 3]
+seAge <- proscat_weighted(x = "idxAge", "dual_se", weight = "vekt", d = dtt, total = TRUE)
+seAge[idxAge, on = "idxAge", alder := i.age3str]
+seAge[is.na(alder), alder := "Totalt"]
+seAge <- seAge[dual_se != 0]
 
 seAge[, type := "Vipe og snus"]
+seAge[, type2 := "se"]
+
+colx <- c("idxAge", "weighted_count", "sum")
+seAge[, (colx) := NULL]
+data.table::setnames(seAge, "dual_se", "dual")
+
+seWide <- dcast(seAge, alder ~ dual, value.var = "percentage")
 
 # Combi av alle 3
-rseAge <- calc_percentage_total_ci(dt,
-                               outcome_var = "combiCase",
-                               group_vars = "age3",
-                               denominator_var = "popSub",
-                               weight_var = "vekt",
-                               include_total = FALSE
-                               )
 
+dtt[combi3 == 3, combiCase := 3]
+rseAge <- proscat_weighted(x = "idxAge", "combiCase", weight = "vekt", d = dtt, total = TRUE)
+rseAge[idxAge, on = "idxAge", alder := i.age3str]
+rseAge[is.na(alder), alder := "Totalt"]
+rseAge <- rseAge[combiCase != 0]
 
 rseAge[, type := "Vape, snus og sigarettar"]
+rseAge[, type3 := "rse"]
+colx <- c("idxAge", "weighted_count", "sum")
+rseAge[, (colx) := NULL]
+data.table::setnames(rseAge, "combiCase", "dual")
 
 
 dualDTAge <- data.table::rbindlist(list(rsAge, reAge, seAge, rseAge))
+
+dualCat <- data.table(
+  dual = c(1,3),
+  dualStr = c("dual", "trippel")
+)
+
+dualDTAge[dualCat, on = "dual", dualstr := i.dualStr]
+
+# # -------------------------------
+# # 2. Reshape to wide format
+# # -------------------------------
+# dtt_wide <- dcast(dualDTAge, alder ~ type2 + dual, value.var = "percentage")
+
+# # -------------------------------
+# # 3. Build stacked chart
+# # -------------------------------
+# highchart() |>
+#   hc_chart(type = "column") |>
+#   hc_xAxis(categories = dtt_wide$alder) |>
+#   hc_plotOptions(column = list(stacking = "normal")) |>
+#   hc_add_series(name = "Snus og sigarettar", data = dtt_wide$rs_1, stack = "stack1") |>
+#   hc_add_series(name = "Vape, snus og sigarettar", data = dtt_wide$rs_3, stack = "stack1") |>
+#   hc_add_series(name = "Vipe og sigarettar", data = dtt_wide$re_1, stack = "stack2") |>
+#   hc_add_series(name = "Vape, snus og sigarettar", data = dtt_wide$re_3, stack = "stack2") |>
+#   hc_add_series(name = "Vipe og snus", data = dtt_wide$se_1, stack = "stack3") |>
+#   hc_add_series(name = "Vape, snus og sigarettar", data = dtt_wide$se_3, stack = "stack3") |>
+#   hc_add_series(name = "Vape, snus og sigarettar", data = dtt_wide$rse_3, stack = "stack4") |>
+#   hc_title(text = "Stacked Column Chart (highcharter)") |>
+#   hc_yAxis(title = list(text = "Value")) |>
+#   highcharter::hc_exporting(
+#       enabled       = TRUE,
+#       filename      = "highdir-figure",
+#       accessibility = list(enabled = TRUE)
+#     )
+
+
+# spec_d <- hd_spec(data = dualDTAge, x = "alder", y = "percentage", group = "type")
+
+# hd_make(spec_d, "stacked_column", stack = "type")
